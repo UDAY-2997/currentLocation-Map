6364be7d2f6ca8caaece820d7e86d8bb3ff22cc0
U-Dev <udaythakur.shely@gmail.com>
2023-04-06 16:44:18 -0500

    map added with functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -14,52 +14,31 @@ const button = document.getElementById("map");
 mapboxgl.accessToken =
   "pk.eyJ1IjoidWRheXZlZXJzaW5naCIsImEiOiJjbGc1OTZ5YXcwMTZ4M3JtcGM0YzR1Z3NxIn0.qSuijAxuTvfagY1o35OcMg";
 
-navigator.geolocation.getCurrentPosition();
-
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://style/mapbox/streets-v12",
+navigator.geolocation.getCurrentPosition(getLocation, errorHandler, {
+  enableHighAccuracy: true,
 });
 
-// function getLocation(position) {
-//   console.log(position);
+function getLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
 
-//   setupMap([position.coords.longitude, position.coords.latitude]);
+function errorHandler() {
+  setupMap([49.8954, -97.1385]);
+}
 
-//   // const { latitude, longitude } = position.coords;
-//   // window.location.assign(
-//   //   `https://www.openstreetmap.org/#map=19/${latitude}/${longitude}`
-//   // );
-// }
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v12?optimize=true",
+    center: center,
+    zoom: 10,
+  });
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
 
-// function errorHandler() {
-//   setupMap([49.8954, 97.1385]);
-//   // console.log("Unable to retrieve your location ");
-// }
+  const directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
 
-// function setupMap(center) {
-//   const map = new mapboxgl.Map({
-//     container: "map",
-//     style: "mapbox://styles/mapbox/streets-v12 optimize=true",
-//     center: center,
-//     zoom: 10,
-//   });
-//   const nav = new mapboxgl.NavigatorControl();
-//   map.addControl(nav);
-
-//   var directions = new MapboxDirections({
-//     accessToken: mapboxgl.accessToken,
-//   });
-
-//   map.addControl(directions, "top-left");
-// }
-
-// button.addEventListener("click", () => {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(getLocation, errorHandler, {
-//       enableHighAccuracy: true,
-//     });
-//   } else {
-//     console.log(`Geolocation is not supported by your browser`);
-//   }
-// });
+  map.addControl(directions, "top-left");
+}
